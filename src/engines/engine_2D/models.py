addd8a191c652059d68bf11a505e3c08f3877a06
Mathieu Marquis <mamar828@ulaval.ca>
2024-03-16 18:54:51 -0400

    Model traces

--- src/engines/engine_2D/models.py
+++ src/engines/engine_2D/models.py
@@ -10,11 +10,13 @@ class Base_model:
         self.repr = pg.Rect(position[0], screen.get_height() - position[1], *scale)
         self.scale = scale
         self.color = color
+        self.trace = []
         self.update()
 
     def move(self, new_position):
         # Reverse the y position because the y axis is inverted in pygame
-        self.repr.topleft = new_position[0] - self.scale[0] // 2, self.screen.get_height() - (new_position[1] + self.scale[1] // 2)
+        self.repr.center = new_position[0], self.screen.get_height() - new_position[1]
+        self.trace.append((new_position[0], self.screen.get_height() - new_position[1]))
 
     @staticmethod
     def get_random_color():
@@ -29,3 +31,9 @@ class Rectangle(Base_model):
 class Circle(Base_model):
     def update(self):
         pg.draw.ellipse(surface=self.screen, color=self.color, rect=self.repr)
+        other_repr = self.repr.copy()
+        other_repr.width /= 7
+        other_repr.height /= 7
+        for trace in self.trace:
+            other_repr.center = trace[0], trace[1]
+            pg.draw.ellipse(surface=self.screen, color=self.color, rect=other_repr)
