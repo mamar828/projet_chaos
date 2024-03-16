b3a0b8acebd287e4a5e40584e1287b34b7bb9b47
Mathieu Marquis <mamar828@ulaval.ca>
2024-03-16 11:44:06 -0400

    Coordinates reversing for pygame models

--- src/engines/engine_2D/models.py
+++ src/engines/engine_2D/models.py
@@ -6,13 +6,15 @@ class Base_model:
     def __init__(self, screen, position=(0,0), scale=(1,1), color="white", instance=None):
         self.instance = instance
         self.screen = screen
-        self.repr = pg.Rect(*position, *scale)
+        # Reverse the y position because the y axis is inverted in pygame
+        self.repr = pg.Rect(position[0], screen.get_height() - position[1], *scale)
         self.scale = scale
         self.color = color
         self.update()
 
     def move(self, new_position):
-        self.repr.topleft = new_position[0] - self.scale[0] // 2, new_position[1] - self.scale[1] // 2
+        # Reverse the y position because the y axis is inverted in pygame
+        self.repr.topleft = new_position[0] - self.scale[0] // 2, self.screen.get_height() - (new_position[1] + self.scale[1] // 2)
 
     @staticmethod
     def get_random_color():
