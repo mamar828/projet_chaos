a020616af92498a8d05ec5e74ab57fd5d5548223
FelixDesroches <117761543+FelixDesroches@users.noreply.github.com>
2024-03-16 15:04:18 -0400

    added test file and removed forgotten print

--- src/systems/base_system.py
+++ src/systems/base_system.py
@@ -133,7 +133,6 @@ class BaseSystem:
             x_max = getattr(stop, axes_names[0])
             y_min = getattr(start, axes_names[1])
             y_max = getattr(stop, axes_names[1])
-            print(x_max, x_min)
             for i, body in enumerate(list_of_massive_bodies):
                 width = (x_max-x_min) if (x_max-x_min) > 0 else (y_max-y_min)
                 height = (y_max - y_min) if (y_max - y_min) > 0 else (x_max - x_min)
